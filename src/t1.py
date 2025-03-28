eb6c27db57271f59d6d41716bf35e9d40ea7d709
frdmmm <faridm0741@gmail.com>
2025-03-28 16:13:18 +0700

    optimization

---
+++ src/t1.py
@@ -0,0 +1,14 @@
+from FFNN import FFNN
+import numpy as np
+nn = FFNN(N_layer=3, 
+          loss='mse', 
+          activation=['linear', 'relu', 'sigmoid'], 
+          N_neuron_layer=[2, 4, 1], weight_method = 'uniform')
+np.random.seed(41)
+X = np.random.rand(100, 2)
+y = np.random.rand(100, 1)
+
+nn.fit(X, y, epochs=100, learning_rate=0.01, batch_size=16, verbose=True)
+
+predictions = nn.predict(X)
+nn.visualize_selected_layers([0, 1,2])
